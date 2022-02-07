f10ba29d29253eb0c0e5f8397d6f4b883327bd1c
Hybxn <ivan.mgarcia125@gmail.com>
2022-02-07 19:48:34 +0100

    Creacion de la interfaz MonoplazaRepository

---
+++ src/main/java/com/pilotos/repositories/MonoplazaRepository.java
@@ -0,0 +1,14 @@
+package com.pilotos.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pilotos.models.Monoplaza;
+import com.pilotos.models.Piloto;
+
+public interface MonoplazaRepository extends JpaRepository<Monoplaza, Long>{
+	
+	List<Monoplaza> findByPilto(Piloto piloto);
+
+}
